8602b41f1d07a06249fae70311ced446e3d1b297
Anton Tsingel <atsingel@VRP.local>
2019-10-08 16:10:33 +0300

    Parsing headers

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ var server = http.createServer(function(req,res){
    var parsedUrl = url.parse(req.url,true);
    
    var path = parsedUrl.pathname;
-   var trimmedPath = path.replace(/^\/+|\/+$/g,'')
+   var trimmedPath = path.replace(/^\/+|\/+$/g,'');
    
    var queryStringObject = parsedUrl.query;
 
    var method = req.method.toLowerCase();
+
+
+   var headers = req.headers;
+
+
+
     
     res.end('Hello world\n');
 
-    console.log('Request received on path: '+trimmedPath+ ' with this method: '+ method+ ' and with these query parameters: ',queryStringObject);
+    console.log('Request received with these headers: ',headers);
 });
 
 server.listen(3000, function(){
