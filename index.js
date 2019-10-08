1a37a2c977f7681a17a481158401286c9af4953d
Anton Tsingel <atsingel@VRP.local>
2019-10-08 15:35:08 +0300

    Parsing request paths

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function(req,res){
+   
+   var parsedUrl = url.parse(req.url,true);
+   
+   var path = parsedUrl.pathname;
+   var trimmedPath = path.replace(/^\/+|\/+$/g,'')
+   
+    
     res.end('Hello world\n');
+
+    console.log('Request received on path: '+trimmedPath);
 });
 
 server.listen(3000, function(){
