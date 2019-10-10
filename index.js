44ff6af3a563e79a9271e6e451ad3ae6b21677e5
Anton Tsingel <atsingel@VRP.local>
2019-10-10 18:50:53 +0300

    Background workers. first part

--- index.js
+++ index.js
@@ -1,103 +1,16 @@
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-//var _data = require('./lib/data');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
-
-// _data.delete('test','newFile',function(err){
-//     console.log('this was the error',err);
-// });
+var server = require('./lib/server');
+var workers = require('./lib/workers');
 
 
-/*
-Connecting to the twilio
-helpers.sendTwilioSms('4158375309','Hello', function(err){
-    console.log('this was the error',err);
-});
-*/
+var app = {};
 
-var httpServer = http.createServer(function(req,res){
-  unifiedServer(req,res);
-});
+app.init = function(){
+  server.init();
 
-httpServer.listen(config.httpPort, function(){
-    console.log("The server is listenining on port "+config.httpPort+" in "+config.envName+" mode");
-});
-
-var httpsServerOptions = {
-    'key' : fs.readFileSync('./https/key.pem'),
-    'cert' : fs.readFileSync('./https/cert.pem')
-};
-var httpsServer = https.createServer(httpsServerOptions, function(req,res){
-    unifiedServer(req,res);
-  });
-
-
-httpsServer.listen(config.httpsPort, function(){
-    console.log("The server is listenining on port "+config.httpsPort+" in "+config.envName+" mode");
-});
-
-
-var unifiedServer = function(req,res){
-
-   var parsedUrl = url.parse(req.url,true);
-   
-   var path = parsedUrl.pathname;
-
-   var trimmedPath = path.replace(/^\/+|\/+$/g,'');
-   
-   var queryStringObject = parsedUrl.query;
-
-   var method = req.method.toLowerCase();
-
-   var headers = req.headers;
-
-   var decoder = new StringDecoder('utf-8');
-   var buffer = '';
-   req.on('data', function(data){
-       buffer += decoder.write(data);
-   });
-   req.on('end',function(){
-       buffer += decoder.end();
-
-       var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-       
-       var data = {
-            'trimmedPath' : trimmedPath,
-            'queryStringObject' : queryStringObject,
-            'method' : method,
-            'headers' : headers,
-            'payload' : helpers.parseJsonToObject(buffer)
-       };
-
-       chosenHandler(data, function(statusCode, payload){
-        statusCode = typeof(statusCode) == 'number' ? statusCode: 200;
-
-        payload =typeof(payload) == 'object' ? payload: {};
-
-        var payloadString = JSON.stringify(payload);
-
-
-        res.setHeader('Content-type', 'application/json');
-        res.writeHead(statusCode);
-        res.end(payloadString);
-
-              
-
-       console.log('Returning this response: ',statusCode, payloadString); 
-       });
-
-   });
-    
+  workers.init();
 };
 
-var router = {
-    'ping' : handlers.ping,
-    'users' : handlers.users,
-    'tokens' : handlers.tokens,
-    'checks' : handlers.checks
-};
+app.init();
+
+
+module.exports = app;
