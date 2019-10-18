370ed71c613a9101b8402be6c7662193ed3d9945
Anton Tsingel <atsingel@VRP.local>
2019-10-18 12:17:26 +0300

    TLS server and client side

---
+++ misc/tls-client.js
@@ -0,0 +1,20 @@
+var tls = require('tls');
+var fs = require('fs');
+var path = require('path');
+
+var options = {
+    'ca' : fs.readFileSync(path.join(__dirname,'/../https/cert.pem'))
+};
+
+
+var outboundMessage = 'ping';
+
+var client = tls.connect(6000,options,function(){
+    client.write(outboundMessage);
+});
+
+client.on('data',function(inboundMessage){
+    var messageString = inboundMessage.toString();
+    console.log("I wrote "+outboundMessage+" and they said "+messageString);
+    client.end();
+});
